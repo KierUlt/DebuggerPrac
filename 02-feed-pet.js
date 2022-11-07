27a7fd1041b20611f9fe9d9af4da331d27108afe
Paul <pjulmeus@gmail.com>
2022-11-07 18:25:34 -0500

    2 solved

--- 02-feed-pet.js
+++ 02-feed-pet.js
@@ -8,9 +8,11 @@ of foods that you have fed that pet.
 */
 
 function feedPet(name) {
-  const foods = [];
+  let foods = [];
   return (food) => {
-    return "Fed " + name + " " + foods.push(food) + ".";
+    foods.push(food)
+
+    return "Fed " + name + " " + foods.join(", ") + ".";
   }
 }
 
